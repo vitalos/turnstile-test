5f2b68101c7354ac7aa4c32dddd040e93d9adebc
vitalos <luke32113@gmail.com>
2026-03-10 16:15:00 -0400

    will this fix the base route?

--- projects/turnstile-test/src/app/app.routes.ts
+++ projects/turnstile-test/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Form } from './form/form';
 import { Success } from './success/success';
 
 export const routes: Routes = [
-    { path: '/turnstile-test', component: Form },
+    { path: '/turnstile-test/', component: Form },
     { path: '/turnstile-test/success', component: Success }
 ];

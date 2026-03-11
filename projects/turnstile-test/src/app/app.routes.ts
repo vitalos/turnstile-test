9eaf2c301050837cd7fabd4e1e42a8e2a9890040
vitalos <luke32113@gmail.com>
2026-03-11 08:24:01 -0400

    ughhhhhhhhhhhhhh

--- projects/turnstile-test/src/app/app.routes.ts
+++ projects/turnstile-test/src/app/app.routes.ts
@@ -4,10 +4,10 @@ import { Success } from './success/success';
 import { App } from './app';
 
 export const routes: Routes = [
-    { path: 'turnstile-test', component: App, children: [
+    { path: '', component: App, children: [
         { path: 'form', component: Form },
         { path: 'success', component: Success },
         { path: '', redirectTo: 'form', pathMatch: 'full' },
         { path: '**', redirectTo: 'form', pathMatch: 'full' },
-    ]},
+    ]}
 ];

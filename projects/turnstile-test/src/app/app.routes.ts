6e1cd8ce37b475979372632d4615fa672a058e41
vitalos <luke32113@gmail.com>
2026-03-11 11:15:48 -0400

    simplify routes

--- projects/turnstile-test/src/app/app.routes.ts
+++ projects/turnstile-test/src/app/app.routes.ts
@@ -4,10 +4,6 @@ import { Success } from './success/success';
 import { App } from './app';
 
 export const routes: Routes = [
-    { path: '', component: App, children: [
-        { path: 'form', component: Form },
-        { path: 'success', component: Success },
-        { path: '', redirectTo: 'form', pathMatch: 'full' },
-        { path: '**', redirectTo: 'form', pathMatch: 'full' },
-    ]}
+    { path: '', component: Form },
+    { path: 'success', component: Success }
 ];

ee048c714caeeed39a970dea641b5ccbd9dff211
vitalos <luke32113@gmail.com>
2026-03-11 11:24:46 -0400

    set default build config to development

--- projects/turnstile-test/src/app/app.routes.ts
+++ projects/turnstile-test/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { Form } from './form/form';
 import { Success } from './success/success';
-import { App } from './app';
 
 export const routes: Routes = [
     { path: '', component: Form },

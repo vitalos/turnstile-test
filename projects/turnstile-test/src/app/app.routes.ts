0e4df1e4d2108d09511c36a4fc61b22d0f19e2f1
vitalos <luke32113@gmail.com>
2026-03-11 08:18:23 -0400

    routes broke again

--- projects/turnstile-test/src/app/app.routes.ts
+++ projects/turnstile-test/src/app/app.routes.ts
@@ -4,8 +4,9 @@ import { Success } from './success/success';
 import { App } from './app';
 
 export const routes: Routes = [
-    { path: '', component: App },
-    { path: 'form', component: Form },
-    { path: 'success', component: Success },
-    { path: '**', redirectTo: 'form' }
+    { path: 'turnstile-test', component: App, children: [
+        { path: 'form', component: Form },
+        { path: 'success', component: Success },
+        { path: '**', redirectTo: 'form' }
+    ]},
 ];
